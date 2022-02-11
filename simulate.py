8f54780521f6294589506ac2a925686203181d30
cello <marcello_tania@yahoo.com>
2022-02-11 23:32:24 +0100

    add stepSimulation

--- simulate.py
+++ simulate.py
@@ -1,3 +1,12 @@
 import pybullet as p
+import time
 
-pass
+
+physicsClient = p.connect(p.GUI)
+
+for x in range(1000):
+    p.stepSimulation()
+    time.sleep( 1/60 )
+    print(x)
+
+p.disconnect()
