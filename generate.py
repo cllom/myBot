fe52dfce864bba541bf16efaffcc11fb28d79f34
cello <marcello_tania@yahoo.com>
2022-04-06 11:47:49 +0200

    add motors

--- generate.py
+++ generate.py
@@ -1,7 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-
-
 l = 1
 w = 1
 h = 1
@@ -23,6 +21,14 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5] , size=[l,w,h])
     pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,0])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[l,w,h])
+
+    '''
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[l,w,h])
+    pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[l,w,h])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[l,w,h])
+    '''
     pyrosim.End()
 
 Create_World()
